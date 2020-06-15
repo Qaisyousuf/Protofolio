49ffb781483a7655dd6999996f936904be855a22
Qais Yousuf <mr.qais.yousuf@gmail.com>
2020-06-15 13:27:54 +0200

    CodePageCreated

---
+++ QaisYousuf.Models/RequstingUIDesign.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QaisYousuf.Models
+{
+    public class RequstingUIDesign:EntityBase
+    {
+        public string MainTitle { get; set; }
+
+        public string Content { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string ButonText { get; set; }
+
+        public string ButtonUrl { get; set; }
+
+    }
+}
---
+++ QaisYousuf.Models/RequstingUIForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace QaisYousuf.Models
+{
+    public class RequstingUIForm:EntityBase
+    {
+        
+        public string ComnayName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Mobile { get; set; }
+
+        public string NumberOfEmployee { get; set; }
+
+        public string CurrentWebSiteUrl { get; set; }
+
+        public DateTime RequestDate { get; set; }
+
+        public DateTime AppointmentDate { get; set; }
+
+        public string IdeaLink { get; set; }
+
+        public int CompanyNameId { get; set; }
+        [ForeignKey("CompanyNameId")]
+        public virtual TypeOfCompany TypeOfCompany { get; set; }
+
+    }
+}
