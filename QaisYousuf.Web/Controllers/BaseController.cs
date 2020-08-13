1be830b6a733f19f3f78492fd51ec4a371a8f2fd
Qais Yousuf <mr.qais.yousuf@gmail.com>
2020-08-13 15:01:26 +0200

    BaseViewModelFixed

---
+++ QaisYousuf.Web/Controllers/BaseController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QaisYousuf.Data.Interfaces;
+using QaisYousuf.Services;
+using Unity;
+using QaisYousuf.ViewModels;
+
+namespace QaisYousuf.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        [Dependency]
+        public IUnitOfWork uow { get; set; }
+
+        [Dependency]
+        public IAuthenticationServices authservices { get; set; }
+
+        protected override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            ViewResult result = filterContext.Result as ViewResult;
+
+            if(result !=null)
+            {
+                BaseViewModel baseViewModel = ViewData.Model as BaseViewModel;
+
+                if(baseViewModel !=null)
+                {
+                    var site = uow.Context.Settings.FirstOrDefault();
+
+                    var siteSetting = uow.Context.Settings.FirstOrDefault(x => x.Id == site.Id);
+
+                    baseViewModel.SiteName = siteSetting.SiteName;
+                    baseViewModel.SiteOwner = siteSetting.SiteOwner;
+                    baseViewModel.GoogleSiteVerfication = siteSetting.GoogleSiteVerfication;
+                    baseViewModel.GoogleAds = siteSetting.GoogleAds;
+                    baseViewModel.SiteLastUpdate = siteSetting.SiteLastUpdate;
+                    baseViewModel.UpdatedBy = siteSetting.UpdatedBy;
+                    baseViewModel.BaseTitle = siteSetting.UpdatedBy;
+                    baseViewModel.BaseContent = siteSetting.UpdatedBy;
+                    baseViewModel.Home = siteSetting.Home;
+                    baseViewModel.HomeUrl = siteSetting.HomeUrl;
+                    baseViewModel.About = siteSetting.About;
+                    baseViewModel.AboutUrl = siteSetting.AboutUrl;
+                    baseViewModel.UIUX = siteSetting.UIUX;
+                    baseViewModel.UIUXURl = siteSetting.UIUXURl;
+                    baseViewModel.Code = siteSetting.Code;
+                    baseViewModel.CodeUrl = siteSetting.CodeUrl;
+                    baseViewModel.Contact = siteSetting.Contact;
+                    baseViewModel.ContactUrl = siteSetting.ContactUrl;
+                    baseViewModel.Support = siteSetting.Support;
+                    baseViewModel.SupportUrl = siteSetting.SupportUrl;
+                    baseViewModel.Profile = siteSetting.Profile;
+                    baseViewModel.ProfileUrl = siteSetting.ProfileUrl;
+                    baseViewModel.CopyrightFooter = siteSetting.CopyrightFooter;
+                    baseViewModel.DesignBy = siteSetting.DesignBy;
+                   
+                }
+            }
+        }
+    }
+}
--- QaisYousuf.Web/Controllers/HomeController.cs
+++ QaisYousuf.Web/Controllers/HomeController.cs
@@ -20,59 +20,26 @@ namespace QaisYousuf.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(new PageViewModel());
         }
-        public ActionResult GetHomeBanner()
+
+        [ChildActionOnly]
+        public ActionResult PartialMenu()
         {
-            var banner = uow.HomeBannerRepository.GetAll();
+            var context = uow.Context;
+            var menus = context.Menus;
 
-
-            List<HomeBannerViewModel> viewmodel = new List<HomeBannerViewModel>();
-
-            foreach (var item in banner)
+            foreach (var menu in menus)
             {
-                viewmodel.Add(new HomeBannerViewModel
-                {
-                    Id = item.Id,
-                    Title = item.Title,
-                    SubTitle = item.SubTitle,
-                    BackgroundImage = item.BackgroundImage,
-                    SliderImagesUrl = item.SliderImagesUrl,
-                    ButtonUrl = item.ButtonUrl,
-                    ButtonText = item.ButtonText
-
-                });
+                context.Entry(menu).Collection(s => s.SubMenu).Query().Where(x => x.ParentId == menu.Id);
             }
-            return Json(new { data = viewmodel }, JsonRequestBehavior.AllowGet);
+
+            var menuSubmenus = menus.AsNoTracking().ToList();
+
+            context.Dispose();
+
+            return PartialView(menuSubmenus);
         }
 
-        [HttpGet]
-        public ActionResult Create()
-        {
-            return View(new HomeBannerViewModel());
-        }
-
-        [HttpPost]
-        public JsonResult Create(HomeBannerViewModel viewmodel)
-        {
-            if (ModelState.IsValid)
-            {
-                HomeBanner homebanner = new HomeBanner
-                {
-                    Id = viewmodel.Id,
-                    Title = viewmodel.Title,
-                    SubTitle = viewmodel.SubTitle,
-                    BackgroundImage = viewmodel.BackgroundImage,
-                    SliderImagesUrl = viewmodel.SliderImagesUrl,
-                    ButtonUrl = viewmodel.ButtonUrl,
-                    ButtonText = viewmodel.ButtonText
-
-                };
-                uow.HomeBannerRepository.Add(homebanner);
-                uow.Commit();
-
-            }
-            return Json(new { success = true, message = "Saved Successfully" }, JsonRequestBehavior.AllowGet);
-        }
     }
 }
