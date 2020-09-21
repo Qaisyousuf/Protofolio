3819f112c05808bca982cba49deff7ee439737a6
Qais Yousuf <mr.qais.yousuf@gmail.com>
2020-09-22 01:03:01 +0200

    ContactBannerFronEndCompleted

---
+++ QaisYousuf.ViewModels/ListOfContact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QaisYousuf.ViewModels
+{
+    public class ListOfContact:BaseViewModel
+    {
+        public List<ContactBannerViewModel> ListContactBannerViewmodel { get; set; }
+        public List<ContactDetailsViewModel> ListContactDetiailsViewModel { get; set; }
+    }
+}
---
+++ QaisYousuf.Web/Controllers/ContactController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QaisYousuf.ViewModels;
+
+namespace QaisYousuf.Web.Controllers
+{
+    public class ContactController : BaseController
+    {
+        [Route("ContactUs")]
+        public ActionResult Index(string slug)
+        {
+            var contactBanner = uow.ContactBannerRepository.GetAll();
+
+            List<ContactBannerViewModel> viewmodel = new List<ContactBannerViewModel>();
+
+
+            foreach (var item in contactBanner)
+            {
+                viewmodel.Add(new ContactBannerViewModel
+                {
+                    Id=item.Id,
+                    MainTitle=item.MainTitle,
+                    SubTitle=item.SubTitle,
+                    ButtonUrl=item.ButtonUrl,
+                    ButtonText=item.ButtonText,
+                    ImageUrl=item.ImageUrl,
+                });
+            }
+
+            var contactDetails = uow.ContactDetialsRepository.GetAll();
+
+            List<ContactDetailsViewModel> detailsViewModel = new List<ContactDetailsViewModel>();
+
+            foreach (var ContactItem in contactDetails)
+            {
+                detailsViewModel.Add(new ContactDetailsViewModel
+                {
+                    Id=ContactItem.Id,
+                    Title=ContactItem.Title,
+                    HomeAddress=ContactItem.HomeAddress,
+                    Moible=ContactItem.Moible,
+                    Email=ContactItem.Email,
+                    WorkingTime=ContactItem.WorkingTime,
+                    WorkingData=ContactItem.WorkingTime,
+
+                });
+            }
+
+            ListOfContact AllListofContact = new ListOfContact
+            {
+                ListContactBannerViewmodel=viewmodel,
+                ListContactDetiailsViewModel=detailsViewModel,
+            };
+            return View(AllListofContact);
+        }
+    }
+}
