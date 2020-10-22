de127ec81c1747793ea1761591f7142794660b4b
Qais Yousuf <mr.qais.yousuf@gmail.com>
2020-10-22 17:45:01 +0200

    TechnologyFrontPageCompleted

---
+++ QaisYousuf.ViewModels/ListOfCodePages.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QaisYousuf.ViewModels
+{
+    public  class ListOfCodePages:BaseViewModel
+    {
+        public List<CodeBannerViewModel> ListOfCodeBannerViewModel { get; set; }
+        public List<WebProgrammingToolsViewModel> ListOfWebProgramming { get; set; }
+        public List<UIUXToolsViewModel> ListUIUXToolsViewModel { get; set; }
+        public List<DesignCodeViewModel> ListCodeDesignViewModel { get; set; }
+    }
+}
---
+++ QaisYousuf.Web/Controllers/TechnologyToolsController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QaisYousuf.Data.Interfaces;
+using QaisYousuf.ViewModels;
+
+namespace QaisYousuf.Web.Controllers
+{
+    public class TechnologyToolsController : BaseController
+    {
+        
+        [Route("Technologies")]
+        public ActionResult Index()
+        {
+            var codeBanner = uow.CodeBannerRepository.GetAll();
+
+            List<CodeBannerViewModel> viewmodel = new List<CodeBannerViewModel>();
+
+            foreach (var item in codeBanner)
+            {
+                viewmodel.Add(new CodeBannerViewModel
+                {
+                    Id=item.Id,
+                    MainTitle=item.MainTitle,
+                    SubTitle=item.SubTitle,
+                    ButtonUrl=item.ButtonUrl,
+                    ButtonText=item.ButtonText,
+                    ImageUrl=item.ImageUrl,
+                });
+            }
+
+            var programmingTools = uow.WebProgrammingToolsRepository.GetAll();
+
+            List<WebProgrammingToolsViewModel> WebProgrammingViewModel = new List<WebProgrammingToolsViewModel>();
+
+            foreach (var WebItem in programmingTools)
+            {
+                WebProgrammingViewModel.Add(new WebProgrammingToolsViewModel
+                {
+                    Id=WebItem.Id,
+                    MainTitle=WebItem.MainTitle,
+                    ImageUrl=WebItem.ImageUrl,
+                    IconUrl=WebItem.IconUrl,
+                    ProgramTitle=WebItem.ProgramTitle,
+                });
+            }
+
+            var uiUXTools = uow.UIUXToolsRepository.GetAll();
+
+            List<UIUXToolsViewModel> UIUXToolsViewModel = new List<UIUXToolsViewModel>();
+
+            foreach (var item in uiUXTools)
+            {
+                UIUXToolsViewModel.Add(new ViewModels.UIUXToolsViewModel
+                {
+                    Id=item.Id,
+                    MainTitle=item.MainTitle,
+                    IconUrl=item.IconUrl,
+                    ProgramTitle=item.ProgramTitle,
+                    ImageUrl=item.ImageUrl
+
+                });
+            }
+
+            var desingCode = uow.DesignCodeSectionRepository.GetAll();
+
+            List<DesignCodeViewModel> DesignCodeViewModel = new List<DesignCodeViewModel>();
+
+            foreach (var item in desingCode)
+            {
+                DesignCodeViewModel.Add(new ViewModels.DesignCodeViewModel
+                {
+                    Id = item.Id,
+                    Title = item.Title,
+                    ImageUrl = item.ImageUrl,
+                });
+            }
+
+            ListOfCodePages CodeList = new ListOfCodePages
+            {
+                ListOfCodeBannerViewModel=viewmodel,
+                ListOfWebProgramming=WebProgrammingViewModel,
+                ListUIUXToolsViewModel=UIUXToolsViewModel,
+                ListCodeDesignViewModel=DesignCodeViewModel,
+            };
+
+            return View(CodeList);
+        }
+    }
+}
