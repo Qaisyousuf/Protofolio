c5e6b3259919e1f8812b5268a3112b5ae90d906a
Qais Yousuf <mr.qais.yousuf@gmail.com>
2020-10-25 02:19:01 +0200

    UIUXPageStarted

---
+++ QaisYousuf.ViewModels/ListUIUX.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QaisYousuf.ViewModels
+{
+    public class ListUIUX:BaseViewModel
+    {
+        public List<UIBannerViewModel> ListUIUXBannerViewModel { get; set; }
+        public List<UIProcessViewModel> ListOfUIProcessViewModel { get; set; }
+
+    }
+}
---
+++ QaisYousuf.Web/Controllers/UIUXController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using QaisYousuf.ViewModels;
+
+namespace QaisYousuf.Web.Controllers
+{
+    public class UIUXController :BaseController
+    {
+        [Route("UIUX")]
+        public ActionResult Index()
+        {
+            var uiUXPage = uow.UIBannerRepository.GetAll();
+
+            List<UIBannerViewModel> UIUXBannerViewModle = new List<UIBannerViewModel>();
+
+            foreach (var item in uiUXPage)
+            {
+                UIUXBannerViewModle.Add(new UIBannerViewModel
+                {
+                    Id=item.Id,
+                    MainTitle=item.MainTitle,
+                    SubTitle=item.SubTitle,
+                    ButtonUrl=item.ButtonUrl,
+                    ButtonText=item.ButtonText,
+                    ImageUrl=item.ImageUrl,
+                });
+            }
+
+
+            var uiProcess = uow.UIProcessRepository.GetAll();
+
+            List<UIProcessViewModel> uIprocessViewModel = new List<UIProcessViewModel>();
+
+            foreach (var item in uiProcess)
+            {
+                uIprocessViewModel.Add(new UIProcessViewModel
+                {
+                    Id=item.Id,
+                    MainTitle=item.MainTitle,
+                    SubTitle=item.SubTitle,
+                    Content=item.Content,
+                    ImageUrl=item.ImageUrl,
+                });
+            }
+            ListUIUX UIUXViewModel = new ListUIUX
+            {
+                ListUIUXBannerViewModel = UIUXBannerViewModle,
+                ListOfUIProcessViewModel=uIprocessViewModel,
+                
+            };
+
+            return View(UIUXViewModel);
+        }
+    }
+}
